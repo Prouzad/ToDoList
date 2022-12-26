17501f47935251faedcf97a0aeb8698d20a0d126
Abdulla <94592483+Prouzad@users.noreply.github.com>
2022-12-26 15:46:39 +0500

    mistake

--- to-do-list/src/components/TaskBox/Task/TaskList.tsx
+++ to-do-list/src/components/TaskBox/Task/TaskList.tsx
@@ -17,23 +17,10 @@ function TaskList(props: { tasks: ITasks, callback: (id: number)=>void, idx: num
 		const arr = state?.tasks?.map((item) => item._id === props.idx ? {...item, done: e.target.checked} : item ).sort(function(x, y) {
 			return Number(x.done) - Number(y.done);
 	 });
-		console.log(arr)
-
 		dispatch({
 			type: 'removeTask',
 			tasks: arr
 		})
-		
-		// if(e.target.checked){
-		// 	dispatch({
-		// 		type: 'done'
-		// 	})
-		// }else if(e.target.checked){
-		// 	dispatch({
-		// 		type: 'not done'
-		// 	})
-		// }
-
 	}
 
   return (
@@ -43,7 +30,7 @@ function TaskList(props: { tasks: ITasks, callback: (id: number)=>void, idx: num
 				<div style={props.tasks.done ? {color: grey[700], textDecoration: 'line-through', textDecorationThickness: '0.2rem'} : {color: '#426ea8'}} className={style.name}>{props.tasks.name}</div>
 			</div>
 			<div className={style.secondBox}>
-				<DeleteForeverIcon sx={props.tasks.done ? {color: grey[700]} : {color: red[400]}} style={{cursor: 'pointer'}} onClick={(e)=> props.callback(props.idx)}/>
+				<DeleteForeverIcon sx={props.tasks.done ? {color: grey[700]} : {color: red[400]}} style={{cursor: 'pointer'}} onClick={()=> props.callback(props.idx)}/>
 				<EditIcon sx={props.tasks.done ? {color: grey[700]} : {color: blue[900]}} style={{cursor: 'pointer'}}/>
 			</div>
     </div>
